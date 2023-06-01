1d5883e67069fceae493f4ab1993698b38f8276f
Wauplin <lucainp@gmail.com>
2023-06-01 12:18:41 +0200

    Test hfh v0.15.0.rc0

--- setup.py
+++ setup.py
@@ -133,7 +133,7 @@ REQUIRED_PKGS = [
     "aiohttp",
     # To get datasets from the Datasets Hub on huggingface.co
     # minimum 0.11.0 to fix 400 Client Error issues
-    "huggingface-hub>=0.11.0,<1.0.0",
+    "huggingface-hub==v0.15.0.rc0",
     # Utilities from PyPA to e.g., compare versions
     "packaging",
     # To parse YAML metadata from dataset cards
